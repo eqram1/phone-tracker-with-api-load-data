5a5e4577debff0d4935cd8eef5f27e11db841bdb
eqram1 <abmeqramhossain@gmail.com>
2023-02-10 19:54:51 +0600

    added url loadphones data in app.js

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const loadPhones = async () => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+}
+loadPhones();
