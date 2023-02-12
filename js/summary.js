a6fbd7de9e7420fc0d433bfcc7c03415313aaa96
eqram1 <abmeqramhossain@gmail.com>
2023-02-12 20:54:07 +0600

    added summary note

---
+++ js/summary.js
@@ -0,0 +1,20 @@
+/*
+FETCH-4steps
+1.fetch must provide url.this url must be static or dynamic.
+2.How to convert fetch promise return to json
+3.How to convet json to data
+4.[cool headed] :
+users:
+-->array of objects
+-->object with property users
+-->object with a property called data
+nested objects
+--------------------------------
+DOM manipulation:
+
+1.get something from the DOM
+2.create element to append to the DOM
+3.dynamically load data based on id
+-----------------------------------
+array-->foreach,map,find,filter,template string
+*/ 
